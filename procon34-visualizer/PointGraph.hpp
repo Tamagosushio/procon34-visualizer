9eae9a155f6a8cb2edae345170a2cec28d885c6d
tamagosushi <mitachi.ts75@gmail.com>
2023-10-27 19:56:26 +0900

    [WIP] 得点のグラフ

---
+++ procon34-visualizer/PointGraph.hpp
@@ -0,0 +1,15 @@
+﻿# pragma once
+# include <Siv3D.hpp> // Siv3D v0.6.12
+# include "Data.hpp"
+
+
+class PointGraph {
+public:
+	PointGraph(void);
+	PointGraph(const Array<int>& ary);
+	void draw(const Point &point, const Size &size, const int &limit) const;
+	void set_background(const Color &color);
+private:
+	Array<int> points;
+	Color color_background = HSV(0, 0, 0, 0);
+};
